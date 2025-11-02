b71d8ff32d88476997dbd5a556676385aec4420b
Boldecca <muteterirebecca60@gmail.com>
2025-11-02 07:31:37 +0200

    added content in src/components/ClientCard.tsx

---
+++ src/components/ClientCard.tsx
@@ -0,0 +1,18 @@
+// ClientCard.tsx
+import React from "react";
+import type { Client } from "../models";
+
+type Props = {
+  client: Client;
+};
+
+export const ClientCard: React.FC<Props> = ({ client }) => {
+  return (
+    <div className="p-3 border rounded">
+      <h3 className="font-semibold">{client.name}</h3>
+      <p>{client.country}</p>
+      <p>{client.email ?? <em>No email provided</em>}</p>
+      <small className="text-sm text-gray-500">ID: {client.id}</small>
+    </div>
+  );
+};
